a77130a2f8dec03dbe39c09e55bdfb2f96b27e8c
root <naresh.sakinala@outlook.com>
2023-01-21 15:09:35 +0000

    Using React Slick - Slider : to display planetsList

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import Slider from 'react-slick'
+import PlanetItem from '../PlanetItem'
+import './index.css'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+
+  return (
+    <div className="planetsSlider_container">
+      <div className="responsive_container">
+        <h1 className="planetsSlider_heading">PLANETS</h1>
+        <Slider>
+          {planetsList.map(eachPlanet => (
+            <PlanetItem key={eachPlanet.id} planetDetails={eachPlanet} />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  )
+}
+
+export default PlanetsSlider
